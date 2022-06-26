ab7af2c24a9fea7ae63a9819c73f2b45ec2e33db
Rod111xd <rodrigojato@hotmail.com>
2022-06-26 09:22:01 -0300

    ListaRedo e Undo

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 
-from msilib.schema import ReserveCost
-
-
 INPUT_FILE = "in.txt"
 
 # Ler entrada e retornar operações
@@ -13,8 +10,11 @@ class Log:
 class Recovery:
     def __init__(self, input_file=None):
         self.Log = Log()
+        self.listaRedo = []    # Ids de transações commited
+        self.listaUndo = []    # Ids de transações ativas
         if input_file:
             self.loadInput(input_file)
+        self.analisarLog()
 
     def loadInput(self, input_file):
         f = open(input_file, "r")
@@ -33,8 +33,32 @@ class Recovery:
         except:
             print("Entrada inválida!")
 
+    def analisarLog(self):
+        # Transações que commitaram vão para a listaRedo
+        # Transações que não commitaram vão para a listaUndo
+        transactions = set()
+        transactions_commited = set()
+        for h in self.Log.histories:
+            tr = h[2]
+            op = h[3]
+            transactions.add(tr)
+            if op == 'c':
+                transactions_commited.add(tr)
+        self.listaRedo = list(transactions_commited)
+        self.listaUndo = list(transactions - transactions_commited)
+
+
+
+    def recover(self):
+
+        log = self.Log.histories
+        log_inv = log[::-1]
+        
+
+
+
 def printHistories(histories):
-    for i, op in enumerate(histories):
+    for op in histories:
         t = len(op)
         out = "[ "
         for j, el in enumerate(op):
@@ -54,7 +78,7 @@ def main():
     print("Entrada:")
     printHistories(recovery.Log.histories)
 
-
+    recovery.recover()
 
 main()
 
